31c0fe305072a25de6142fb248590066d6199ec6
Renato Firmino Santos da Silva <renatofss16@gmail.com>
2026-02-05 10:02:46 -0300

    Início do curso de JavaScript

---
+++ README.md
@@ -0,0 +1,2 @@
+# Curso de JavaScript
+Repositório para estudos de JavaScript.
---
+++ secao_5/forIn.js
@@ -0,0 +1,15 @@
+const notas = [6,8,7,5,6,2]
+for(i in notas){
+    //console.log(notas[i])
+}
+
+const pessoa = {
+    nome: 'ana',
+    sobrenome: 'maria',
+    idade: 29,
+    peso: 64 
+}
+
+for(i in pessoa){
+    console.log(pessoa[i])
+}
